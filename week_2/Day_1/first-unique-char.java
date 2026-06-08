6ebe09bddd38b6fa685aaf48640879a9b71cba57
Suryansh-4201 <Suryanshtripathi4201@gmail.com>
2026-06-08 23:07:49 +0530

    changes in code week2, day1

---
+++ week_2/Day_1/first-unique-char.java
@@ -0,0 +1,21 @@
+package week_2.Day_1;
+class Solution {
+    public int firstUniqChar(String s) {
+
+        int[] count = new int[26];
+
+        // Count frequency of each character
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        // Find first character with frequency 1
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ week_2/Day_1/ransome-note.java
@@ -0,0 +1,21 @@
+package week_2.Day_1;
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            count[ransomNote.charAt(i) - 'a']--;
+
+            if (count[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
